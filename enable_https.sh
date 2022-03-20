e49e0d1a73f97f13047ceee302e38c300179c6dc
Alex Musayev <alex.musayev@gmail.com>
2022-03-20 14:54:46 +0700

    Use the variables

--- enable_https.sh
+++ enable_https.sh
@@ -1,1 +1,1 @@
-ansible-playbook ./enable_letsencrypt.yml --extra-vars "letsencrypt_domain=$LETSENCRYPT_DOMAIN letsencrypt_email=$LETSENCRYPT_EMAIL"
+ansible-playbook ./enable_https.yml --extra-vars "letsencrypt_domain=$LETSENCRYPT_DOMAIN letsencrypt_email=$LETSENCRYPT_EMAIL"
